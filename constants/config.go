119a1d5605cce1c4cf2ca722100514f47f76613f
Dilip Kumar <dilipkumar.dev.official@gmail.com>
2026-04-26 11:53:09 +0530

    feat: add configuration constants for environment variable keys

--- constants/config.go
+++ constants/config.go
@@ -1,13 +1,12 @@
 package constants
 
-import "os"
 
-var (
-	EnvClientId     = os.Getenv("CLIENT_ID")
-	EnvClientSecret = os.Getenv("CLIENT_SECRET")
-	EnvCallbackUrl  = os.Getenv("CALLBACK_URL")
-	EnvFrontendUrl  = os.Getenv("FRONTEND_URL")
-	EnvClientPat    = os.Getenv("CLIENT_PAT")
-	EnvLLMApiKey    = os.Getenv("LLM_API_KEY")
-	EnvLLMBaseUrl   = os.Getenv("LLM_BASE_URL")
+const (
+	EnvClientId     = "CLIENT_ID"
+	EnvClientSecret = "CLIENT_SECRET"
+	EnvCallbackUrl  = "CALLBACK_URL"
+	EnvFrontendUrl  = "FRONTEND_URL"
+	EnvClientPat    = "CLIENT_PAT"
+	EnvLLMApiKey    = "LLM_API_KEY"
+	EnvLLMBaseUrl   = "LLM_BASE_URL"
 )
